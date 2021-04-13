7b8e34a20c0afcd52487138e0528084bfc383a7c
malidzhikov <m.alidzhikov@astoundcommerce.com>
2021-04-13 18:03:31 +0300

    GRIDWORLD - solve policy improvement/iteration

--- 01.Dynamic Programming/policyEvaluation.py
+++ 01.Dynamic Programming/policyEvaluation.py
@@ -19,13 +19,35 @@ def policy_eval(env, policy):
                 v = 0
                 for pi, pp in enumerate(policy[xi][yi]):
                     sp,r = env.step([xi, yi],env.A[pi])
-                    next_v = V[sp[0]][sp[1]] if not sp is None else 0
-                    v += (pp*(r + next_v))
+                    v += pp*(r + V[sp[0]][sp[1]])
                 delta = max(delta, np.abs(v - V[xi][yi]))
                 V[xi][yi] = v
         print(delta)
         if delta < theta:
             break;
     print(V)
+    return V
     
-policy_eval(env, policy)
+V = policy_eval(env, policy)
+
+def policy_improvement(env, policy, V):
+    greedy_policy = np.zeros(env.S.shape + (4,))
+    while True:
+        policy_stable = True
+        for xi, xv in enumerate(env.S):
+            for yi, s in enumerate(xv):
+                a = np.argmax(policy[xi][yi])
+                a_vals = []
+                for pi, pp in enumerate(policy[xi][yi]):
+                    sp,r = env.step([xi, yi],env.A[pi])
+                    a_vals.append(r + V[sp[0]][sp[1]])
+                a_greedy = np.argmax(a_vals)
+                greedy_policy[xi][yi][a_greedy] = 1
+                if not a == a_greedy:
+                    policy_stable = False
+        if policy_stable:
+            break;
+        return greedy_policy
+    
+greedy_policy = policy_improvement(env, policy, V)
+V = policy_eval(env, greedy_policy)
