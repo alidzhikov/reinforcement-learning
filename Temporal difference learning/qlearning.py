62f61079bd88d999c33d6ef91fb88421c5072f37
alidzhikov <m.alidzhikov2630@gmail.com>
2021-04-20 16:28:02 +0300

    QLEARNING - solution

--- Temporal difference learning/qlearning.py
+++ Temporal difference learning/qlearning.py
@@ -1,35 +1,22 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 from collections import defaultdict
-from windygridworld import WindyGridworldEnv
+from cliff_walking import CliffWalkingEnv
 import plotutils
 import sys
 
-env = WindyGridworldEnv()
+env = CliffWalkingEnv()
 # UP, RIGHT, DOWN, LEFT
 actions = [0, 1, 2, 3]
+
 def get_action(s, Q, epsilon):
     policy = np.ones(env.action_space.n) * epsilon / env.action_space.n
     A = np.argmax(Q[s])
     policy[A] += 1-epsilon
     return round(np.random.choice(actions, p=policy))
 
-def sarsa(env, num_episodes, discount_factor=1.0, alpha=0.5, epsilon=0.1):
-    """
-    SARSA algorithm: On-policy TD control. Finds the optimal epsilon-greedy policy.
-    
-    Args:
-        env: OpenAI environment.
-        num_episodes: Number of episodes to run for.
-        discount_factor: Gamma discount factor.
-        alpha: TD learning rate.
-        epsilon: Chance the sample a random action. Float betwen 0 and 1.
-    
-    Returns:
-        A tuple (Q, stats).
-        Q is the optimal action-value function, a dictionary mapping state -> action values.
-        stats is an EpisodeStats object with two numpy arrays for episode_lengths and episode_rewards.
-    """
+
+def q_learning(env, num_episodes, discount_factor=1.0, alpha=0.5, epsilon=0.1):
     
     # The final action-value function.
     # A nested dictionary that maps state -> (action -> action-value).
@@ -41,8 +28,8 @@ def sarsa(env, num_episodes, discount_factor=1.0, alpha=0.5, epsilon=0.1):
         episode_rewards=np.zeros(num_episodes))
     
     for x in range(num_episodes):
-        if(x+1) % 100 == 0:
-            print("\rEpisode {}/{}".format(x+1,num_episodes), end="")
+        if (x + 1) % 10 == 0:
+            print("\rEpisode {}/{}".format(x + 1, num_episodes), end="")
             sys.stdout.flush()
         s = env.reset()
         a = get_action(s, Q, epsilon)
@@ -50,7 +37,7 @@ def sarsa(env, num_episodes, discount_factor=1.0, alpha=0.5, epsilon=0.1):
         ep_length = 0
         while not done:
             s_prm, r, done, _ = env.step(a)
-            a_prm = get_action(s_prm, Q, epsilon)
+            a_prm = np.argmax(Q[s_prm]) 
             ep_length += 1
             Q[s][a] += alpha * (r + discount_factor * Q[s_prm][a_prm] - Q[s][a])
             s = s_prm
@@ -60,6 +47,6 @@ def sarsa(env, num_episodes, discount_factor=1.0, alpha=0.5, epsilon=0.1):
         
     return Q, stats
 
-Q, stats = sarsa(env, 200)
+Q, stats = q_learning(env, 500)
 
 plotutils.plot_episode_stats(stats)
