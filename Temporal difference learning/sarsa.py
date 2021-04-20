56927a2b4cd553723ab73ee7c0bee43f9e7eec23
alidzhikov <m.alidzhikov2630@gmail.com>
2021-04-20 15:01:08 +0300

    TD - set up

---
+++ Temporal difference learning/sarsa.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+import matplotlib
+import numpy as np
+from collections import defaultdict
+from windygridworld import WindyGridworldEnv
+
+env = WindyGridworldEnv()
+
+print(env.reset())
+env.render()
+
+print(env.step(1))
+env.render()
+
+
+print(env.step(1))
+env.render()
+
+
+print(env.step(1))
+env.render()
+
+
+print(env.step(1))
+env.render()
+
+def sarsa(env, num_episodes, discount_factor=1.0, alpha=0.5, epsilon=0.1):
+    """
+    SARSA algorithm: On-policy TD control. Finds the optimal epsilon-greedy policy.
+    
+    Args:
+        env: OpenAI environment.
+        num_episodes: Number of episodes to run for.
+        discount_factor: Gamma discount factor.
+        alpha: TD learning rate.
+        epsilon: Chance the sample a random action. Float betwen 0 and 1.
+    
+    Returns:
+        A tuple (Q, stats).
+        Q is the optimal action-value function, a dictionary mapping state -> action values.
+        stats is an EpisodeStats object with two numpy arrays for episode_lengths and episode_rewards.
+    """
+    
+    # The final action-value function.
+    # A nested dictionary that maps state -> (action -> action-value).
+    Q = defaultdict(lambda: np.zeros(env.action_space.n))
+    
+    # Keeps track of useful statistics
+    stats = plotting.EpisodeStats(
+        episode_lengths=np.zeros(num_episodes),
+        episode_rewards=np.zeros(num_episodes))
